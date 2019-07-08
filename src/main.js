9517a65c784dc545e2304d04c18cc438d4b89188
Daniel Hongel, Erin Deuel, Marina Poltorak, Sam Cook and Shawna Duvall <sam@>
2019-07-08 16:45:44 -0700

    add key event handlers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import $ from 'jquery';
+import './styles.css';
+import { Player } from './map.js';
+import { keyDownHandler } from './movement.js';
+import { keyUpHandler } from './movement.js';
+
+let fox = new Player;
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
 
 $(document).ready(function () {
-
+  var mapStuff = document.getElementById('map');
+  
 
 });
