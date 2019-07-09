14cbfba3e914c165031c190146ff7301e8096a5f
Student <example@example.com>
2019-07-09 15:49:44 -0700

    add male yell and fox growl sounds and related code.

--- src/main.js
+++ src/main.js
@@ -9,16 +9,22 @@ let fox = new Player;
 //sound related imports and variables-----------------------------------------
 
 //import sounds
-let eatChickenSound = require('./audio/Chicken-Fuss-in-Coop.mp3')
-let backgroundSound = require('./audio/countryside-ambiance.wav')
+let eatChickenSound = require('./audio/Chicken-Fuss-in-Coop.mp3');
+let backgroundSound = require('./audio/countryside-ambiance.wav');
+let farmerYellSound = require('./audio/male-offended-hey-shout-full.mp3');
+let foxGrowlSound = require ('./audio/foxGrowl.wav');
 
 //define sounds in new Audio objects for value adjustments such as volume
 let eatChicken = new Audio(eatChickenSound);
 let backgroundMusic = new Audio(backgroundSound);
+let farmerYell = new Audio(farmerYellSound);
+let foxGrowl = new Audio(foxGrowlSound);
 
 //volume adjustments for soundfiles
 eatChicken.volume = .2;
 backgroundMusic.volume = .1;
+farmerYell.volume = .2;
+foxGrowl.volume=.2;
 
 //solution found on stack overflow that helps with a loop issue with some browsers
 //the if statement helps make this solution adapt to other browser features as
@@ -48,6 +54,5 @@ backgroundMusic.pause();
 setTimeout(function () {
   backgroundMusic.play();
 }, 150);
-
 
 });
