65448fb9941981c788ace3d003679873115a6b12
Student <example@example.com>
2019-07-10 15:41:17 -0700

    add all the images and background.

--- src/grid.js
+++ src/grid.js
@@ -15,34 +15,55 @@ export class Level {
     this.columns = 0;
     this.rows = 0;
     this.tileSize = 40;
-    }
-    drawTheMap() {
-      console.log('in draw the map');
+  }
+  drawTheMap() {
+    console.log('in draw the map');
+    let roofCenterSideways = document.getElementById("roof-center-sideways");
+    let roofEdgeUp = document.getElementById("roof-edge-up");
+    let roofEdgeDown = document.getElementById("roof-edge-down");
+    let treeLine = document.getElementById("treeline");
+    let roofLeft = document.getElementById("roof-left");
+    let roofCenter = document.getElementById("roof-center");
+    let roofRight = document.getElementById("roof-right");
+    let pinkTree = document.getElementById("pink-tree");
+    let bush = document.getElementById("bush");
 
-      let xCoord = 0;
-      let yCoord = 0;
-      let ctx = document.getElementById('map').getContext('2d');
-      for (let i = 0; i < this.map.length; i++) {
-        if (i !== 0) {
-          if (i % this.columns !== 0) {
-            xCoord += this.tileSize;
-          } else {
-            xCoord = 0;
-            yCoord += this.tileSize;
-          }
+    let xCoord = 0;
+    let yCoord = 0;
+    let ctx = document.getElementById('map').getContext('2d');
+    for (let i = 0; i < this.map.length; i++) {
+      if (i !== 0) {
+        if (i % this.columns !== 0) {
+          xCoord += this.tileSize;
+        } else {
+          xCoord = 0;
+          yCoord += this.tileSize;
         }
-        if (this.map[i] === 1) {
-          ctx.fillStyle = '#000';
-          ctx.fillRect(xCoord, yCoord, this.tileSize, this.tileSize);
-        } else if (this.map[i] === 'v') {
-          ctx.fillStyle = 'blue';
-          ctx.fillRect(xCoord, yCoord, this.tileSize, this.tileSize);
-        } else if (this.map[i] !== 0 && this.map[i] !== 1) {
-          ctx.fillStyle = 'darksalmon';
-          ctx.fillRect(xCoord, yCoord, this.tileSize, this.tileSize);
-        } else if (this.map[i] === 0) {
-          ctx.clearRect(xCoord, yCoord, this.tileSize, this.tileSize);
-        }
+      }
+      if (this.map[i] === 1) {
+        ctx.drawImage (treeLine, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 2) {
+        ctx.drawImage (roofLeft, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 3) {
+        ctx.drawImage (roofCenter, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 4) {
+        ctx.drawImage (roofRight, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 5) {
+        ctx.drawImage (roofEdgeUp, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 6) {
+        ctx.drawImage (roofCenterSideways, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 7) {
+        ctx.drawImage (roofEdgeDown, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 8) {
+        ctx.drawImage (pinkTree, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 9) {
+        ctx.drawImage (bush, xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 0) {
+        ctx.clearRect(xCoord, yCoord, this.tileSize, this.tileSize);
+      } else if (this.map[i] === 'v') {
+        ctx.fillStyle = 'rgba(255, 255, 153, 0.1)';
+        ctx.fillRect(xCoord, yCoord, this.tileSize, this.tileSize);
       }
     }
   }
+}
